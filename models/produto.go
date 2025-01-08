8686c097669a8d60004f4effded2124da5670d9c
Lucio Pintanel <lm.pintanel@gmail.com>
2025-01-08 17:46:51 -0300

    - Criado a documentação do arquivo models/produto. #9

--- models/produto.go
+++ models/produto.go
@@ -2,9 +2,10 @@ package models
 
 import "time"
 
+// Produto representa a estrutura do produto no banco de dados.
 type Produto struct {
-	ID          int       `json:"id"`
-	Descricao   string    `json:"descricao"`
-	DataCriacao time.Time `json:"data_criacao"`
-	Categoria   string    `json:"categoria"`
+	ID          int       `json:"id"`           // ID do produto.
+	Descricao   string    `json:"descricao"`    // Descrição do produto.
+	DataCriacao time.Time `json:"data_criacao"` // Data de criação do produto.
+	Categoria   string    `json:"categoria"`    // Categoria do produto.
 }
