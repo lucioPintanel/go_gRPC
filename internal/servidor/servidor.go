0133471d5fca59ee11671a81b7815045610fc9a2
Lucio Pintanel <lm.pintanel@gmail.com>
2025-01-08 17:44:12 -0300

    - Criado a documentação do arquivo servidor. #9

--- internal/servidor/servidor.go
+++ internal/servidor/servidor.go
@@ -11,18 +11,22 @@ import (
 	"google.golang.org/grpc"
 )
 
+// Server representa o servidor gRPC.
 type Server struct {
-	pb.UnimplementedProdutoServiceServer
+	pb.UnimplementedProdutoServiceServer // Implementa o serviço Produto.
 }
 
+// NewGRPCServer cria uma nova instância do servidor gRPC.
 func NewGRPCServer() *grpc.Server {
 	s := grpc.NewServer()
 	return s
 }
 
+// Start inicia o servidor gRPC.
 func (s *Server) Start() {
 	logs.Logger.Info("Iniciando o servidor na porta de acesso 50051")
 
+	// Configura o listener na porta 50051.
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		logs.Logger.Sugar().Errorf("Falha em inicializar listen")
@@ -30,9 +34,10 @@ func (s *Server) Start() {
 	}
 	grpcServer := NewGRPCServer()
 
-	// Registrar o controlador do produto apenas aqui
+	// Registra o controlador do produto no servidor gRPC.
 	controllers.RegisterProdutoController(grpcServer)
 
+	// Inicia o servidor gRPC.
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		logs.Logger.Sugar().Errorf("Falha em inicializar o servidor")
